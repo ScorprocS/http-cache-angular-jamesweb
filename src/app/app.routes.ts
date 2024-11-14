5f5bac0cdebf02d8bb6f575a761cb3d24b817bf9
Alex <a.blanc100@laposte.net>
2024-11-14 14:07:55 +0100

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'',
+        loadComponent:()=>import('./program-list/program-list.component').then(m=>m.ProgramListComponent)
+    },
+    {
+        path:'programs/:id',
+        loadComponent:()=>import('./program-detail/program-detail.component').then(m=>m.ProgramDetailComponent)
+    }
+];
